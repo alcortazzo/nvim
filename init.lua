75486cc80f755a6a8915de9170f3b2aeae82ed1f
alcortazzo <ipodarock@gmail.com>
2025-03-13 14:49:42 +0200

    Configure lazygit integration

--- init.lua
+++ init.lua
@@ -140,6 +140,27 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 require('lazy').setup({
   'natecraddock/workspaces.nvim', -- Workspaces
 
+  { -- LazyGit integration
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+
   { -- GitHub Copilot
     'github/copilot.vim',
     lazy = false,
